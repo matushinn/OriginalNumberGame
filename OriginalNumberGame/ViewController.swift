0ea04258f1390ff2ea53159d5894a99b13f74e88
shotaro-ooe <shoooe97@gmail.com>
2018-11-09 16:31:35 +0900

    計算機能終了

---
+++ OriginalNumberGame/ViewController.swift
@@ -0,0 +1,275 @@
+//
+//  ViewController.swift
+//  Dentaku
+//
+//  Created by 大江祥太郎 on 2018/10/28.
+//  Copyright © 2018年 shotaro. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+   
+    @IBOutlet weak var leftLabel: UILabel!
+    
+    
+    @IBOutlet weak var rightLabel: UILabel!
+    
+    
+    @IBOutlet weak var calcLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var maruImageView: UIImageView!
+    
+    @IBOutlet weak var batsuImageView: UIImageView!
+    @IBOutlet weak var startLabel: UIImageView!
+    
+    @IBOutlet weak var startButtonLabel: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    var calc:[String] = ["➕","➖","✖️"]
+    
+    var questionNum :Int = 0
+    
+    var result:Int = 0
+    
+    var leftNumber :Int = 0
+    var rightNumber :Int = 0
+    
+    var answer:Int = 0
+    
+    var timer:Timer!
+    var count:Double = 0.0
+    
+    
+    func vibrate() {
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    }
+//    問題を出す関数
+    func showQuestion(){
+        leftNumber=Int(arc4random_uniform(10))
+        rightNumber=Int(arc4random_uniform(10))
+        leftLabel.text = String(leftNumber)
+        rightLabel.text = String(rightNumber)
+        
+        let calculation = Int( arc4random_uniform(UInt32(calc.count)) )
+        calcLabel.text = calc[calculation]
+        
+    }
+    func answerQuestion(){
+        if result == answer{
+            //正解
+            vibrate()
+            /*
+            UIView.animate(withDuration: 1, animations: {
+                self.maruImageView.alpha = 0
+            }, completion: { finished in
+                self.maruImageView.removeFromSuperview()
+            })*/
+            showQuestion()
+            questionNum += 1
+//            if questionNum == 2{
+//                let ResultViewController = storyboard!.instantiateViewController(withIdentifier: "nextView")
+//                present(ResultViewController, animated: true, completion: nil)
+            
+//            }
+            
+        }else{
+            //不正解
+            vibrate()
+            /*
+            UIView.animate(withDuration: 1, animations: {
+                self.batsuImageView.alpha = 0
+            }, completion: { finished in
+                self.batsuImageView.removeFromSuperview()
+            })*/
+            
+        }
+        answer = 0
+        answerLabel.text = "0"
+    }
+    
+    
+    
+    
+    @IBAction func okButton(_ sender: Any) {
+        
+        
+        if calcLabel.text == "➕"{
+            result = leftNumber + rightNumber
+        }
+        if calcLabel.text == "➖"{
+            result = leftNumber - rightNumber
+        }
+        if calcLabel.text == "✖️"{
+            result = leftNumber * rightNumber
+        }
+        
+        answerQuestion()
+        
+    }
+    
+    @IBAction func cButton(_ sender: Any) {
+        answer = 0
+        answerLabel.text = String(answer)
+    }
+    
+    @IBAction func zeroButton(_ sender: Any) {
+        if answerLabel.text != "0"{
+            answer = 10*answer + 0
+        }
+        answerLabel.text = String(answer)
+    }
+    @IBAction func oneButton(_ sender: Any) {
+        if answerLabel.text == "0"{
+            answer = 1
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 1
+        }
+        if answerLabel.text == "-"{
+            answer = -1
+        }
+        answerLabel.text = String(answer)
+        
+    }
+    
+    @IBAction func twoButton(_ sender: Any) {
+        if answerLabel.text == "0"{
+            answer = 2
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 2
+        }
+        if answerLabel.text == "-"{
+            answer = -2
+        }
+        answerLabel.text = String(answer)
+        
+    }
+    
+    @IBAction func threeButton(_ sender: Any) {
+        if answerLabel.text == "0"{
+            answer = 3
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 3
+        }
+        if answerLabel.text == "-"{
+            answer = -3
+        }
+        answerLabel.text = String(answer)
+        
+    }
+    @IBAction func fourButton(_ sender: Any) {
+        if answerLabel.text == "0"{
+            answer = 4
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 4
+        }
+        if answerLabel.text == "-"{
+            answer = -4
+        }
+        answerLabel.text = String(answer)
+        
+    }
+    
+    @IBAction func fiveButton(_ sender: Any) {
+        if answerLabel.text == "0"{
+            answer = 5
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 5
+        }
+        if answerLabel.text == "-"{
+            answer = -5
+        }
+        answerLabel.text = String(answer)
+        
+    }
+    @IBAction func sixButton(_ sender: Any) {
+        
+        
+        if answerLabel.text == "0"{
+            answer = 6
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 6
+        }
+        if answerLabel.text == "-"{
+            answer = -6
+        }
+        answerLabel.text = String(answer)
+        
+    }
+    
+    @IBAction func sevenButton(_ sender: Any) {
+        
+        if answerLabel.text == "0"{
+            answer = 7
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 7
+        }
+        if answerLabel.text == "-"{
+            answer = -7
+        }
+        answerLabel.text = String(answer)
+        
+        }
+
+    @IBAction func eightButton(_ sender: Any) {
+        if answerLabel.text == "0"{
+            answer = 8
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 8
+        }
+        if answerLabel.text == "-"{
+            answer = -8
+        }
+        answerLabel.text = String(answer)
+    }
+
+    
+    @IBAction func nineButton(_ sender: Any) {
+       
+        if answerLabel.text == "0"{
+            answer = 9
+        }
+        if answerLabel.text != "0" && answerLabel.text != "-"{
+            answer = 10*answer + 9
+        }
+        if answerLabel.text == "-"{
+            answer = -9
+        }
+        answerLabel.text = String(answer)
+        
+        }
+    
+    
+    @IBAction func mainasuButton(_ sender: Any) {
+        answerLabel.text = "-"
+    }
+    
+
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showQuestion()
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(UIMenuController.update), userInfo: nil, repeats: true)
+       
+        
+    }
+//    timer
+    func update(){
+        count = count + 0.01
+        timerLabel.text = String(count)
+        
+    }
+    
+}
